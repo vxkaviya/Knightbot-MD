28028918f43753b52c9176d616da7d15451672e1
vxkaviya <rkvk3698@gmail.com>
2025-06-05 20:38:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
+  botName: "KAVEE Bot",
+  botOwner: 'KAVEE Bot', // Your name
   ownerNumber: '94762676277', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
